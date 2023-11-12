62a963fa9dbc22809d0834217b8a2f46cdacfa6f
Miro Palmu <miro.palmu@helsinki.fi>
2023-11-12 09:58:20 +0200

    Modify colors for better visibility

--- lua/plugins/everblush.lua
+++ lua/plugins/everblush.lua
@@ -2,13 +2,21 @@ return {
     "Everblush/nvim",
     name = "everblush",
     config = function()
-        require('everblush').setup({
+        -- Custom comment color for better visibility
+        local comment_color = {
+            fg = "#00FFFF",
+            italic = true
+        }
 
---            override = { Normal = { fg = '#dadada', bg = '#000000' }},
---            transparent_background = true,
---            nvim_tree = {
---                contrast = true,
---            },
+        require('everblush').setup({
+            -- See: https://github.com/Everblush/nvim/blob/main/lua/everblush/theme.lua
+            -- for documentation what can be overridden
+            override = {
+                Comment = comment_color,
+                ["@comment"] = comment_color,
+                -- Custom line number color for better visibility
+                LineNr = { fg = "#C47220" },
+            },
 
         })
 
