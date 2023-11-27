fe927c0af7f4c223b7ed6a0c9995718b77c51139
Miro Palmu <miro.palmu@helsinki.fi>
2023-11-27 11:20:47 +0200

    Customize colors for better visibility

--- lua/plugins/everblush.lua
+++ lua/plugins/everblush.lua
@@ -8,6 +8,11 @@ return {
             italic = true
         }
 
+        -- Everblush pallette color 8 (#2d3437) is not very visible
+        -- so define new one
+        local custom_color8 = "#ffea00"
+        vim.g.terminal_color_8 = custom_color8
+
         require('everblush').setup({
             -- See: https://github.com/Everblush/nvim/blob/main/lua/everblush/theme.lua
             -- for documentation what can be overridden
@@ -18,6 +23,13 @@ return {
                 LineNr = { fg = "#C47220" },
                 -- Custom vertical buffer separator
                 WinSeparator = { fg = "#000000" },
+                -- Cursor line line number
+                CursorLineNr = comment_color,
+                -- Set all which use color 8
+                ["@exception"] = { fg = custom_color8 },
+                ["@float"] = { fg = custom_color8 },
+                ["@text.danger"] = { fg = custom_color8 },
+                ["@text.note"] = { fg = custom_color8 },
             },
 
         })
