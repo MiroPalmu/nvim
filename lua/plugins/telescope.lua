550c6a39c20640d4a94f80cde0ca05adc712c071
Miro Palmu <miro.palmu@helsinki.fi>
2024-01-02 11:30:09 +0200

    Enable optional dependencies for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,13 @@
-local M = { "nvim-telescope/telescope.nvim" }
+local M = { "nvim-telescope/telescope.nvim", branch = "0.1.x" }
 
-M.dependencies = { "nvim-lua/plenary.nvim" }
+M.dependencies = {
+    "nvim-lua/plenary.nvim",
+    "BurntSushi/ripgrep",
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' 
+    },
+}
 
 function M.config()
     local builtin = require('telescope.builtin')
@@ -36,8 +43,15 @@ function M.config()
           --   extension_config_key = value,
           -- }
           -- please take a look at the readme of the extension you want to configure
+            fzf = {
+                fuzzy = true,                    -- false will only do exact matching
+                override_generic_sorter = true,  -- override the generic sorter
+                override_file_sorter = true,     -- override the file sorter
+                case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+            },
+        }
     }
-}
 end
 
 return M
