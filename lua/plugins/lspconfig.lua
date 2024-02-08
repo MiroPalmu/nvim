aa7696cc7dfb5f649b75cdec9f962cef92f40af0
Miro Palmu <miro.palmu@helsinki.fi>
2024-02-08 15:43:35 +0200

    Add LSP autocompletion using nvim-cmp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,11 +1,19 @@
 local M = { "neovim/nvim-lspconfig" }
 
+M.dependencies = {
+    "neovim/nvim-lspconfig",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip",
+}
+
+-- cmp setup at bottom of this function
 function M.config()
     -- Setup language servers.
     local lspconfig = require('lspconfig')
     lspconfig.pyright.setup {}
     lspconfig.clangd.setup{}
-
 
     -- Global mappings.
     -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -81,5 +89,61 @@ function M.config()
             }))
       end,
     })
+
+    ---------------
+    -- cmp setup --
+    ---------------
+    -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    local servers = { 'clangd',  'pyright' }
+    for _, lsp in ipairs(servers) do
+        lspconfig[lsp].setup {
+            -- on_attach = my_custom_on_attach,
+            capabilities = capabilities,
+        }
+    end
+
+    -- nvim-cmp setup
+    local cmp = require 'cmp'
+    local luasnip = require 'luasnip'
+    cmp.setup {
+        snippet = {
+            expand = function(args)
+                luasnip.lsp_expand(args.body)
+            end,
+        },
+        mapping = cmp.mapping.preset.insert({
+            ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+            ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+            -- C-b (back) C-f (forward) for snippet placeholder navigation.
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<CR>'] = cmp.mapping.confirm {
+                behavior = cmp.ConfirmBehavior.Replace,
+                select = true,
+            },
+            ['<Tab>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_next_item()
+                elseif luasnip.expand_or_jumpable() then
+                    luasnip.expand_or_jump()
+                else
+                    fallback()
+                end
+            end, { 'i', 's' }),
+            ['<S-Tab>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_prev_item()
+                elseif luasnip.jumpable(-1) then
+                    luasnip.jump(-1)
+                else
+                    fallback()
+                end
+            end, { 'i', 's' }),
+        }),
+        sources = {
+            { name = 'nvim_lsp' },
+            { name = 'luasnip' },
+        },
+    }
 end
 return M
---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,5 @@
+ return {
+    "hrsh7th/nvim-cmp",
+    event = { "InsertEnter", "CmdlineEnter" },
+    -- Rest of the setup found in lspconfig.lua
+}
