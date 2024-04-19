d79ee2e6eec546583bed0dcce287dfbc2dd9df58
Miro Palmu <email@miropalmu.cc>
2024-04-19 22:07:08 +0300

    Add keybindings for info.vim

--- lua/plugins/info_vim.lua
+++ lua/plugins/info_vim.lua
@@ -1,3 +1,48 @@
-local M = { }
-M.url = 'https://gitlab.com/HiPhish/info.vim.git'
-return M
+return {
+    url = 'https://gitlab.com/HiPhish/info.vim.git',
+    lazy = false,
+    keys = {
+        {
+            "<C-k>",
+            "<Plug>(InfoUp)",
+            mode = "n",
+            ft = "info",
+            desc = "Go to Up node.",
+        },
+        {
+            "<C-h>",
+            "<Plug>(InfoPrev)",
+            mode = "n",
+            ft = "info",
+            desc = "Go to Prev node.",
+        },
+        {
+            "<C-l>",
+            "<Plug>(InfoNext)",
+            mode = "n",
+            ft = "info",
+            desc = "Go to Prev node.",
+        },
+        {
+            "m",
+            "<Plug>(InfoMenu)",
+            mode = "n",
+            ft = "info",
+            desc = "InfoMenu",
+        },
+        {
+            "f",
+            "<Plug>(InfoFollow)",
+            mode = "n",
+            ft = "info",
+            desc = "InfoFollow",
+        },
+        {
+            "o",
+            "<Plug>(InfoGoto)",
+            mode = "n",
+            ft = "info",
+            desc = "InfoGoto",
+        },
+    },
+}
